8d09612e586c1759bb4970db79372a098a0a57be
ricardo44 <ricardo.lin100@gmail.com>
2024-10-31 14:33:33 +0900

    Added the retrain part for selected features

--- Projects/MedicalExamination/survey.py
+++ Projects/MedicalExamination/survey.py
@@ -1,44 +1,57 @@
-#%%
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, auc
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Import File
+# survey_file_terminal = "./Projects/MedicalExamination/data/survey.csv"
 survey_file = "./data/survey.csv"
 survey_data = pd.read_csv(survey_file, delimiter=",")
 data = survey_data.drop(survey_data.columns[0], axis=1)
 
+# Set the file into x and y 
 x_data = data.iloc[:,:-2]
 y_data = data.iloc[:,-1].rename("Result")
 
-#%%
-X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=42)
+# Split train and test data
+X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=77)
 
-model = RandomForestClassifier(n_estimators=100, random_state=77)
+# Set the model adn train 
+model = RandomForestClassifier(n_estimators=100, random_state=77, n_jobs=-1)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 
-#%%
+#Sort and select the feature with the most importance rate
 features = X_test.columns
 importance_df = pd.DataFrame({
     'features' : features,
     'importance' : model.feature_importances_
 })
-
 importance_df = importance_df.sort_values(by='importance', ascending=False, ignore_index=True)
 importance_df.loc[:50,'importance'].sum()
-
 importance_df['cumulative_importance'] = \
     importance_df['importance'].cumsum()
 
+# Set the threshold for cumulative importance and extract the top importance until threshold
 threshold = 0.8
 threshold_index = importance_df[importance_df['cumulative_importance'] > threshold].index[0]
-
 selected_features = importance_df['features'].iloc[:threshold_index].values
-print(len(selected_features))
+print("Current question number: "+len(selected_features))
 
+# Re set our data and train it into model
+X_data_filtered = x_data[selected_features]
+X_train_filtered, X_test_filtered, y_train, y_test = train_test_split(X_data_filtered, y_data, test_size=0.2, random_state=77)
+
+model = RandomForestClassifier(n_estimators=100, 
+    random_state=77)
+model.fit(X_train_filtered, y_train)
+y_pred = model.predict(X_test_filtered)
+accuracy = accuracy_score(y_test, y_pred)
+
+# show the importance into plot
 plt.figure(figsize=(12, 6))
 plt.bar(range(len(importance_df)), importance_df["cumulative_importance"]*100)
 plt.xlabel("Data Point")
