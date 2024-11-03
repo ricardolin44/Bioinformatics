eacb2b816b085ccf8a2678130affdca469a0f9fd
ricardo44 <ricardo.lin100@gmail.com>
2024-11-03 22:13:49 +0900

    Updated

--- Projects/MedicalExamination/survey.py
+++ Projects/MedicalExamination/survey.py
@@ -2,33 +2,56 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, auc
+from sklearn.model_selection import GridSearchCV
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # Import File
-# survey_file_terminal = "./Projects/MedicalExamination/data/survey.csv"
-survey_file = "./data/survey.csv"
-survey_data = pd.read_csv(survey_file, delimiter=",")
+survey_file_terminal = "./Projects/MedicalExamination/data/survey.csv"
+# survey_file = "./data/survey.csv"
+survey_data = pd.read_csv(survey_file_terminal, delimiter=",")
 data = survey_data.drop(survey_data.columns[0], axis=1)
 
 # Set the file into x and y 
-x_data = data.iloc[:,:-2]
+data_extracted = data.iloc[:,-2].str.split('_').str[0].rename('Difficulty')
+data.iloc[:,-2] = data_extracted
+x_data = data.iloc[:,:-1]
 y_data = data.iloc[:,-1].rename("Result")
+# print(data.iloc[:, -2].unique())
 
 # Split train and test data
 X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=77)
 
-# Set the model adn train 
-model = RandomForestClassifier(n_estimators=100, random_state=77, n_jobs=-1)
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+# Set the model and train 
+firstModel = RandomForestClassifier(n_estimators=100, random_state=77, n_jobs=-1)
+firstModel.fit(X_train, y_train)
+y_pred = firstModel.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
+
+
+# model = RandomForestClassifier(random_state=77)
+# param_grid = {
+#     'n_estimators': [200, 250, 300, 350],        # Number of trees in the forest
+#     'max_depth': [None, 2, 5, 10],        # Maximum depth of the tree
+#     'min_samples_split': [2, 3, 4],        # Minimum samples required to split a node
+#     'min_samples_leaf': [1, 2, 3],          # Minimum samples required at each leaf node
+#     'max_features': ['sqrt', 'log2', None]  # Number of features to consider for best split
+# }
+# model = RandomForestClassifier(random_state=77)
+# gridSearchModel = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, scoring='accuracy', n_jobs=-1, verbose=2)
+# gridSearchModel.fit(X_train, y_train)
+# print("Best Parameters:", gridSearchModel.best_params_)
+# print("Best Score:", gridSearchModel.best_score_)
+# best_rf_model = gridSearchModel.best_estimator_
+# y_pred = best_rf_model.predict(X_test)
+# accuracy = accuracy_score(y_test, y_pred)
+# print("Test Set Accuracy:", accuracy)
 
 #Sort and select the feature with the most importance rate
 features = X_test.columns
 importance_df = pd.DataFrame({
     'features' : features,
-    'importance' : model.feature_importances_
+    'importance' : firstModel.feature_importances_
 })
 importance_df = importance_df.sort_values(by='importance', ascending=False, ignore_index=True)
 importance_df.loc[:50,'importance'].sum()
@@ -39,7 +62,7 @@ importance_df['cumulative_importance'] = \
 threshold = 0.8
 threshold_index = importance_df[importance_df['cumulative_importance'] > threshold].index[0]
 selected_features = importance_df['features'].iloc[:threshold_index].values
-print("Current question number: "+len(selected_features))
+# print("Current question number: "+len(selected_features))
 
 # Re set our data and train it into model
 X_data_filtered = x_data[selected_features]
