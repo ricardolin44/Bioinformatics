dcbf59b84b8fe8e8e04d1f6b71987cf07c23b127
ricardo44 <ricardo.lin100@gmail.com>
2024-11-04 23:43:54 +0900

    Predicted first model using auc

--- Projects/MedicalExamination/survey.py
+++ Projects/MedicalExamination/survey.py
@@ -1,15 +1,16 @@
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, auc
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score,precision_score, recall_score, f1_score
 from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import label_binarize
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # Import File
-survey_file_terminal = "./Projects/MedicalExamination/data/survey.csv"
-# survey_file = "./data/survey.csv"
-survey_data = pd.read_csv(survey_file_terminal, delimiter=",")
+# survey_file_terminal = "./Projects/MedicalExamination/data/survey.csv"
+survey_file = "./data/survey.csv"
+survey_data = pd.read_csv(survey_file, delimiter=",")
 data = survey_data.drop(survey_data.columns[0], axis=1)
 
 # Set the file into x and y 
@@ -28,6 +29,28 @@ firstModel.fit(X_train, y_train)
 y_pred = firstModel.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 
+cm = confusion_matrix(y_test, y_pred)
+print("混同行列:\n", cm)
+
+precision = precision_score(y_test, y_pred, average='macro')
+recall = recall_score(y_test, y_pred, average='macro')
+f1 = f1_score(y_test, y_pred, average='macro')
+
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1スコア:", f1)
+
+y_test.unique()
+# ラベルのバイナライズ（マルチクラス対応）
+label_mapping = {'Cluster1': 0, 'Cluster2': 1, 'Cluster3': 2, 'Cluster4': 3, 'Cluster5': 4, 'Cluster6': 5, 'Cluster7': 6}
+y_test_numeric = y_test.map(label_mapping)
+y_test_numeric.unique()
+y_test_binarized = label_binarize(y_test_numeric, classes=[0,1,2,3,4,5,6])
+y_score = firstModel.predict_proba(X_test)
+
+# マクロ平均のAUC
+auc = roc_auc_score(y_test_binarized, y_score, average='macro', multi_class='ovr')
+print("AUC (Macro):", auc)
 
 # model = RandomForestClassifier(random_state=77)
 # param_grid = {
